8eb6cb40e16d4735e110f43331f18eea5ed049dc
David León Salas <108154987+DvLeu@users.noreply.github.com>
2023-02-23 21:45:53 -0600

    Update codeforces486.cpp

--- codeforces486.cpp
+++ codeforces486.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main(){
     long long int n, a;
     cin >> n ;
-    if(n%2 == 0) a = n / 2 ;
-    else a = ((n + 1) / 2 ) * (-1);
+    if(n%2 == 0){
+        a = n / 2 ;
+    } 
+    else {
+        a = ((n + 1) / 2 ) * (-1);
+    }
     cout << a << endl;
-
-
     return 0;
     }
 
